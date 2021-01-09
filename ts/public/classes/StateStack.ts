ae3301c34add45c4a99adc1eed00b670029f97ad
NightshadeX9X <solgaliumz@gmail.com>
2021-01-09 15:36:21 +0100

    added StateStack class

--- ts/public/classes/StateStack.ts
+++ ts/public/classes/StateStack.ts
@@ -1,46 +1,63 @@
-import { Entity } from "../Util.js";
-import Input from "./Input.js";
-import Loader from "./Loader.js";
-import Renderer from "./Renderer.js";
-import RoamState from "./states/RoamState.js";
-import State from "./states/State.js";
-import StartScreenState from './states/StartScreenState.js';
+import Entity from "../interfaces.js";
+import State from "./State.js";
 
 export default class StateStack implements Entity {
 	public states: State[] = [];
 
-	constructor(public loader: Loader) {
-		this.states.push(new StartScreenState(this));
-	}
-
+	/**
+	 * Preloads the state, executes the init function of that state, and then adds it to the `this.states`.
+	 *
+	 * @memberof StateStack
+	 */
 	async push(s: State) {
-		this.states.push(s);
-		await this.top?.preload(this.loader)
+		await s.preload();
+		s.init();
+		this.states = [...this.states, s];
 	}
 
 	pop() {
 		return this.states.pop();
 	}
 
-	get top(): State | undefined {
-		return this.states[this.states.length - 1];
+	/**
+	 * Returns the top state from the stack if no argument provided. If an argument is provided, the nth state from the top (where n is that argument) is returned.
+	 *
+	 */
+	top(nthFromTop = 0) {
+		return this.states[this.states.length - (1 + nthFromTop)];
 	}
 
-	get bottom(): State | undefined {
-		return this.states[0];
+	/**
+	 * Returns the bottom state from the stack if no argument provided. If an argument is provided, the nth state from the bottom (where n is that argument) is returned.
+	 *
+	 */
+	bottom(nthFromBottom = 0) {
+		return this.states[nthFromBottom];
 	}
-	async preload(loader: Loader) {
 
-		this.loader = loader;
-		for await (let s of this.states) {
-			await s.preload(loader);
+	async preload() {
+		for (const s of this.states) {
+			await s.preload();
 		}
 	}
-	update(input: Input): void {
 
-		this.states.filter(s => s.forceUpdate || s === this.top).forEach(s => s.update(input));
+	init() {
+		this.states.forEach(s => s.init());
 	}
-	render(renderer: Renderer): void {
-		this.states.forEach(s => s.render(renderer));
+
+	update() {
+		this.states.forEach(s => {
+			if (s.forceUpdate === false) return;
+			if (s.forceUpdate === true) s.update();
+			if (s === this.top()) s.update();
+		})
 	}
+
+	render() {
+		this.states.forEach(s => {
+			if (s.toRender === false) return;
+			s.update();
+		})
+	}
+
 }
